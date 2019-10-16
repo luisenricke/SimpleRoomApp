531616ee68882a2d551bc1d77ef10958c686a88c
luisenricke <luisevm24@outlook.com>
2019-10-16 15:03:37 -0500

    perf: structure properly the interfaces with functions

--- app/src/main/java/com/luisenricke/simpleroomapp/data/BaseDAO.kt
+++ app/src/main/java/com/luisenricke/simpleroomapp/data/BaseDAO.kt
@@ -33,32 +33,28 @@ interface BaseDAO<X> {
     fun delete(row: X)
 
     /**
-     * Drop all rows existing in the table.
+     * Count rows from the table.
+     * @return the total number of rows.
+     * @see @Query("SELECT count(*) FROM ${DatabaseScheme.name.TABLE}")
      */
-    fun drop()
+    fun count(): Int
 
-    interface ComplementaryOperations<Y> {
-        /**
-         * Count rows from the table.
-         * @return the total number of rows.
-         */
-        fun count(): Int
-
+    interface ComplementaryOperationsWithList<Y> {
         /**
          * Get an list of objects existing in the table.
          * @return list of objects requested.
+         * @see @Query("SELECT * FROM ${DatabaseScheme.name.TABLE}")
          */
         fun get(): List<Y>
+
+        /**
+         * Drop all rows existing in the table.
+         * @see @Query("DELETE FROM ${DatabaseScheme.name.TABLE}")
+         */
+        fun drop()
     }
 
     interface AdditionalOperationWithID<Z> {
-        /**
-         * Get an object with ID existing in the table.
-         * @param id of the row requested.
-         * @return row the object to be requested.
-         */
-        fun filterId(id: Long): Z
-
         /**
          * Update an row with an existing ID in the table and change the data.
          * @param row the object to be updated.
@@ -67,18 +63,18 @@ interface BaseDAO<X> {
         fun update(row: Z)
 
         /**
+         * Get an object with ID existing in the table.
+         * @param id of the row requested.
+         * @return row the object to be requested.
+         * @see @Query("SELECT * FROM ${DatabaseScheme.name.TABLE} WHERE ${DatabaseScheme.name.ID} == :id")
+         */
+        fun filterId(id: Long): Z
+
+        /**
          * Delete row with ID existing in the table.
          * @param id of the row deleted.
+         * @see @Query("DELETE FROM ${DatabaseScheme.name.TABLE} WHERE ${DatabaseScheme.name.ID} == :id")
          */
         fun delete(id: Long)
     }
-
-    /*
-        // Some great implementations
-        @Transaction
-        open fun reset(rows: List<Y>){
-            drop()
-            insert(rows: List<Y>)
-        }
-    */
 }
