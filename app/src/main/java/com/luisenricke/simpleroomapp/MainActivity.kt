bdb5dcf416fb6bfd70b578feec077c94fdb87255
luisenricke <luisevm24@outlook.com>
2019-10-16 15:10:16 -0500

    perf: abstract the process of download and the implentating was in the Activity

--- app/src/main/java/com/luisenricke/simpleroomapp/MainActivity.kt
+++ app/src/main/java/com/luisenricke/simpleroomapp/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.luisenricke.simpleroomapp
 
+import android.graphics.Bitmap
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.luisenricke.simpleroomapp.data.AppDatabase
 import com.luisenricke.simpleroomapp.data.contact.Contact
+import com.luisenricke.simpleroomapp.utils.ImageHelper
 import com.luisenricke.simpleroomapp.utils.OkHttpHelper
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.activity_main.*
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,22 +19,30 @@ class MainActivity : AppCompatActivity() {
     }
 
     private lateinit var list: List<Contact>
-    private lateinit var database: AppDatabase
+    private lateinit var db: AppDatabase
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        database = AppDatabase.getInstance(this)
+        db = AppDatabase.getInstance(this)
 
         val urlimage =
             "https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2FjrTMMG0zJyI%2Fmaxresdefault.jpg"
         val urlimage2 =
             "https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fwallup.net%2Fwp-content%2Fuploads%2F2016%2F05%2F14%2F45171-Samurai_Champloo-Mugen.jpg"
 
-        OkHttpHelper.imageCacheObservable(urlimage, database.cacheDAO())
+        OkHttpHelper.downloadImage(urlimage, db.cacheDAO())
             .subscribeOn(Schedulers.newThread())
             .observeOn(AndroidSchedulers.mainThread())
-            .subscribe(OkHttpHelper.imageCacheObserver(database.cacheDAO()))
+            .doOnComplete {
+                Timber.i("Before -> Rows: ${db.cacheDAO().count()}")
+                val image = db.cacheDAO().pop()?.src?.let { ImageHelper.byteArraytoBitmap(it) }
+                img_db.setImageBitmap(image)
+                Timber.i("After -> Rows: ${db.cacheDAO().count()}")
+            }.doOnError { Timber.e(it) }
+            .subscribe()
+        //val image: Bitmap? = OkHttpHelper.downloadImage(urlimage, db.cacheDAO())
+        //Timber.i("Image: ${image?.byteCount} :: ${image?.width}x${image?.height}")
     }
 }
