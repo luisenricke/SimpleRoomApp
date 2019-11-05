8d8bdc134b5bb90798c01befed72721aecededcd
luisenricke <luisevm24@outlook.com>
2019-11-05 13:43:56 -0600

    build: update project dependencies

--- app/src/main/java/com/luisenricke/simpleroomapp/utils/PermissionImageGalleryImp.kt
+++ app/src/main/java/com/luisenricke/simpleroomapp/utils/PermissionImageGalleryImp.kt
@@ -1,16 +1,18 @@
 package com.luisenricke.simpleroomapp.utils
 
+import android.Manifest
 import android.content.ContentResolver
 import android.content.Intent
 import android.graphics.Bitmap
 import android.provider.MediaStore
+import com.luisenricke.simpleroomapp.Constraints
 import timber.log.Timber
 import java.io.IOException
 
-class OpenGalleryPermission(
-    override var NAME: String,
-    override var CODE_PERMISSION: Int,
-    override var CODE_REQUEST: Int
+class ImageGalleryPermission(
+    override var manifest: MutableCollection<String> = arrayListOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+    override var permissionCode: Int = Constraints.PERMISSION_IMAGE_GALLERY,
+    override var requestCode: Int = Constraints.REQUEST_IMAGE_GALLERY
 ) : Permission {
 
     override fun config(): Intent = Intent(Intent.ACTION_PICK).setType("image/*")
